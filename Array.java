d5760ecbed218d8ec4bb900f48743799d46b75f2
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-11-20 20:59:21 +0530

    it is a java array proggramming

--- Array.java
+++ Array.java
@@ -1,19 +1,33 @@
 import java.util.*;
-public class Array {
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("1 dimensional array");
-        int []arr= new int[5];
-        int i;
-        System.out.println("enter the elements");
-        for(i=0;i<5;i++)
-        {
-            arr[i]=sc.nextInt();
-        }
-        System.out.println("now below the array");
-        for(i=0;i<5;i++)
-        {
-            System.out.println(arr[i]);
-        }
+
+public class Array{
+    public static void main(String args[]){
+            int row, col, sum = 0;
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter the value of row");
+             row = sc.nextInt();
+
+             System.out.println("Enter the value of the col");
+             col = sc.nextInt();
+
+            int arr[][] = new int[row][col];
+            System.out.println("Here the array of the element");
+            for(int i = 0; i < row; i++){
+                for(int j = 0; j < col; j++){
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println("Here the original araay");
+            for(int i = 0; i < row; i++){
+                for(int j = 0; j < col; j++){
+                    System.out.print(arr[i][j]+" ");
+                    sum += arr[i][j];
+                }
+                System.out.println();
+            }
+            System.out.println("the sum of the array element is = "+sum);
+
     }
 }
