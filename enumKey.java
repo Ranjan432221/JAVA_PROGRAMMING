e3e92b11043caba46b1b2da3ca035b6e0c566f4b
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2025-03-24 12:06:27 +0530

    here is the enum and unicode java use in java programming

---
+++ enumKey.java
@@ -0,0 +1,14 @@
+enum Color{
+    red,
+    green,
+    blue;
+}
+
+public class enumKey {
+    public static void main(String args[]) {
+        Color clr = Color.blue;
+      System.out.println(clr);
+      Color cl = Color.red;
+      System.out.println(cl);
+    }
+}
---
+++ unicodeUse.java
@@ -0,0 +1,9 @@
+public class unicodeUse {
+    public static void main(String args[]) {
+        char a = '\u0041';
+        char b = '\u0042';
+
+        System.out.println("the use of unicode is available = "+a);
+        System.out.println("the use of unicode is available = "+b);
+    }
+}
